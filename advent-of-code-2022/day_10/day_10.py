977d0c993fdb1ecf82b35cf3412031d0090b71f4
Guillaume Gandon <guillaume.gandon3@gmail.com>
2022-12-11 16:10:40 +0100

    Improve answer part two readability

--- advent-of-code-2022/day_10/day_10.py
+++ advent-of-code-2022/day_10/day_10.py
@@ -13,9 +13,9 @@ print("Answer part two:")
 line = ""
 for cycle, x in enumerate(register_values):
     if x <= (cycle + 1) % 40 <= x + 2:
-        line += "x"
+        line += "██"
     else:
-        line += "."
+        line += "  "
     if (cycle + 1) % 40 == 0:
         print(line)
         line = ""
