0f25317061e9ef7257d5e5831431c041f8a26727
GuillaumeGandon <guillaume.gandon3@gmail.com>
2024-12-10 07:50:58 +0100

    Add day_10

--- advent-of-code-2024/day_10.py
+++ advent-of-code-2024/day_10.py
@@ -2,19 +2,19 @@ from collections import deque
 
 
 def find_trail_score(start, part_2=False):
-    queue = deque([[start, {start}]])
+    queue = deque([start])
     nb_distinct = 0
     ends = set()
 
     while queue:
-        pos, seen = queue.popleft()
+        pos = queue.popleft()
         for m in (1, -1, 1j, -1j):
             if pos + m in map and map[pos] + 1 == map[pos + m]:
                 if map[pos + m] == 9:
                     ends.add(pos + m)
                     nb_distinct += 1
                 else:
-                    queue.append((pos + m, seen.union({pos + m})))
+                    queue.append(pos + m)
 
     if part_2:
         return nb_distinct
